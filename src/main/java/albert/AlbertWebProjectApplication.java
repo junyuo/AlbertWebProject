2ffa6eab488a87c6be66a5246543f1a0503804d2
albert_kuo <albert_kuo@cht.com.tw>
2016-07-06 11:03:08 +0800

    add spring boot

---
+++ src/main/java/albert/AlbertWebProjectApplication.java
@@ -0,0 +1,12 @@
+package albert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbertWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbertWebProjectApplication.class, args);
+	}
+}
